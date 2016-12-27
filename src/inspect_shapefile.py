3e29f6c8c54a7b8d8d91b515f1d278645bab2f9d
Chris Havlin <chris.havlin@gmail.com>
2016-12-27 14:12:34 -0800

    the xml generation is working well now -- prints field name, data type and a sample of the record values (depending on data type)

--- src/inspect_shapefile.py
+++ src/inspect_shapefile.py
@@ -18,6 +18,7 @@ Copyright (C) 2016  Chris Havlin, <https://chrishavlin.wordpress.com>
 """
 import shapefile
 import xml.etree.cElementTree as ET
+import numpy as np
 
 """
  IMPORT THE SHAPEFILE 
@@ -42,12 +43,12 @@ class field_description(object):
           # find the data type of the field
           self.field_type=None
           shapeid=0
-          print 'searching for non-empty entry...'
+          print 'searching for non-empty entry for', self.fieldname,'...'
           while not self.field_type and shapeid < nshapes-1:
                 rec=sf.record(shapeid)[field_names.index(self.fieldname)]
                 if rec:
-                   self.field_type=type(rec) 
-                   print 'data type found'
+                   self.field_type=str(type(rec)).split("'")[1]
+                   print 'data type found', self.field_type
                 shapeid += 1
    
       def get_unique_rec_values(self,sf):
@@ -63,7 +64,6 @@ class field_description(object):
           self.rec_vals=list()
 
           shape_id = 0
-
           print 'Finding unique record values for',self.fieldname
           for rec in sf.iterRecords():
               # pull out shape geometry and records
@@ -74,18 +74,88 @@ class field_description(object):
                  print shape_id, 'of', nshapes, ' shapes (', pct_comp,'% )'
                  print "  new record value:",rec[field_names.index(self.fieldname)]
                  self.rec_vals.append(rec[field_names.index(self.fieldname)])
+          print 'Completed field name inspection \n'
  
 def create_xml_file(sf,savedir,basename):
     metadata = ET.Element("metadata")
     eainfo = ET.SubElement(metadata, "eainfo")
     detailed = ET.SubElement(eainfo,"detailed",name=basename)
-    attr = ET.SubElement(detailed,"attr")
-    
-    ET.SubElement(attr, "attrlbl").text = "label"
-    ET.SubElement(attr, "attrtype").text = "type"
-    # if string with small number of types
-    ET.SubElement(attr, "attrrange").text = "range"
-    # if a number, just use min/max 
+
+
+    fld = sf.fields[1:]
+    fields0 = [field[0] for field in fld]
+
+    dtypes = list()
+    attr_list = list()
+    for fld in fields0:
+        dtypes.append('')
+        attr_list.append(list())
+
+
+    shapeid=0
+    nshapes=len(sf.shapes())
+    pct_0 = -1
+    for rec in sf.iterRecords():
+        pct_comp=float(int(float(shapeid)/float(nshapes)*10000))/100.
+
+        if np.remainder(int(pct_comp),10)==0 and int(pct_comp) != pct_0:
+           pct_0=pct_comp
+           print shapeid, 'of', nshapes, ' shapes (', pct_comp,'% )'
+
+        # remove the empty fieldnames
+        b= rec[:]
+        field_names=fields0[:]
+        while b.count('')>0:
+              ind=b.index('')
+              field_names.pop(ind)
+              b.pop(ind)
+
+        # check if non-empty fields have a recorded data type, store it if not
+        for fld in field_names:
+            if dtypes[fields0.index(fld)]=='':
+               dtypes[fields0.index(fld)]=str(type(b[field_names.index(fld)])).split("'")[1]
+            
+            if dtypes[fields0.index(fld)]=='str':
+               atts = attr_list[fields0.index(fld)]
+               if b[field_names.index(fld)] not in atts: 
+                  atts.append(b[field_names.index(fld)])
+            else: 
+               atts = attr_list[fields0.index(fld)]
+               if len(atts)==0: 
+                  atts.append(b[field_names.index(fld)])
+                  atts.append(b[field_names.index(fld)])
+               else:
+                  atts[0] = min(atts[0], b[field_names.index(fld)])
+                  atts[1] = max(atts[1], b[field_names.index(fld)])
+               
+        shapeid+=1
+
+    print fields0
+    print dtypes
+
+    for field in fields0:
+        attr = ET.SubElement(detailed,"attr",name=field)
+        ET.SubElement(attr, "attrlbl",name="record label").text = field
+        ET.SubElement(attr, "attrtype",name="data type").text = dtypes[fields0.index(field)]
+
+        atts = attr_list[fields0.index(field)]
+        attsamp_lab='unique str values'
+        if len(atts)>25:
+           attprint=str(atts[0:30]).replace("'","")
+           attsamp_lab='sample of str values'
+        elif dtypes[fields0.index(field)]!= 'str':
+           attprint=str(atts[:]).replace("'","")
+           attsamp_lab='min/max'
+        else:
+           attprint=str(atts[:]).replace("'","")
+           attsamp_lab='unique str values'
+
+        ET.SubElement(attr, "attsamp",name=attsamp_lab).text = attprint
+
+        # if string with small number of types
+#        #ET.SubElement(attr, "attrrange").text = "range"
+#        # if a number, just use min/max 
+#        #  self.field_type=None
     
     tree = ET.ElementTree(metadata)
     tree.write(savedir+basename+"_auto.xml")
@@ -94,7 +164,7 @@ if __name__ == '__main__':
 
    # set the shapefile
    shp_file_base='ex_QMDJXT8DzmqNh6eFiNkAuESyDNCX_osm_line'
-   dat_dir='../shapefiles/denver_maps/grouped_by_geometry_type/'
+   dat_dir='../../learning_shapefiles/shapefiles/denver_maps/grouped_by_geometry_type/'
 
    user_path=raw_input("Do you want to enter the path to a shapefile? (Y/N) ")
    print ' '
@@ -106,42 +176,33 @@ if __name__ == '__main__':
       print 'directory: ',dat_dir
       print 'filename: ',shp_file_base
       
-
-   #shp_file_base='denver_tree_canopy_2013'
-   #dat_dir='../shapefiles/denver_tree_canopy_2013/'
-
    # load the shapefile
-   print ' '
-   print 'Loading shapefile ...'
+   print '\n Loading shapefile ...'
    sf = shapefile.Reader(dat_dir+shp_file_base)
-   print '... shapefile loaded!'
-   print ' '
+   print '... shapefile loaded! \n'
 
-   # pull out the fields
-   fld = sf.fields[1:]
-   field_names = [field[0] for field in fld]
-   print 'Shapefile has the following field names'
-   print field_names
-   print ' '
-   field_of_interest=raw_input("Enter field name to investigate: ")
-   print ' '
-
-   # process the shapefile
-   field_obj=field_description(field_of_interest) # store field name 
-   field_obj.get_field_type(sf) # find field data type
-   field_obj.get_unique_rec_values(sf) # find unique values
-
-   print '---------------------------------------'
-   print 'Shapefile has the following field names'
-   print field_names
-   print ' '
-   print 'The field name',field_obj.fieldname,' is ',field_obj.field_type
-   print 'and has',len(field_obj.rec_vals),'unique values'
-
-   Y_N=raw_input("Display Values? (Y/N) ")
-   if Y_N=='Y':
-      print ' possible values:'
-      print field_obj.rec_vals
+#   # pull out the fields
+#   fld = sf.fields[1:]
+#   field_names = [field[0] for field in fld]
+#   print 'Shapefile has the following field names'
+#   print field_names,'\n'
+#   field_of_interest=raw_input("Enter field name to investigate: ")
+#
+#   # process the shapefile
+#   field_obj=field_description(field_of_interest) # store field name 
+#   field_obj.get_field_type(sf) # find field data type
+#   field_obj.get_unique_rec_values(sf) # find unique values
+#
+#   print '---------------------------------------'
+#   print 'Shapefile has the following field names'
+#   print field_names
+#   print '\n The field name',field_obj.fieldname,' is ',field_obj.field_type
+#   print 'and has',len(field_obj.rec_vals),'unique values'
+#
+#   Y_N=raw_input("Display Values? (Y/N) ")
+#   if Y_N=='Y':
+#      print ' possible values:'
+#      print field_obj.rec_vals
 
    if raw_input("Create XML file? (Y/N) ")=='Y':
       create_xml_file(sf,dat_dir,shp_file_base)
